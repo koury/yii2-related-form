067fe1e43fccc12950a92d740f651dd235588864
anatolii <videoller@gmail.com>
2016-04-03 00:18:19 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# yii2-related-form-widget
+yii2-related-form
+===================
+
+It is widget for melon.ng to clone form elements of related models.
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist tolik505/yii2-related-form "*"
+```
+
+or add
+
+```
+"tolik505/yii2-related-form": "*"
+```
+
+to the require section of your `composer.json` file.
+
+###Javascript Events
+
+```javascript
+
+$(".dynamicform_wrapper").on("beforeInsert", function(e, item) {
+    console.log("beforeInsert");
+});
+
+$(".dynamicform_wrapper").on("afterInsert", function(e, item) {
+    console.log("afterInsert");
+});
+
+$(".dynamicform_wrapper").on("beforeDelete", function(e, item) {
+    if (! confirm("Are you sure you want to delete this item?")) {
+        return false;
+    }
+    return true;
+});
+
+$(".dynamicform_wrapper").on("afterDelete", function(e) {
+    console.log("Deleted item!");
+});
+
+$(".dynamicform_wrapper").on("limitReached", function(e, item) {
+    alert("Limit reached");
+});
+
+```
---
+++ src/RelatedFormAsset.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @link      https://github.com/tolik505/yii2-related-form
+ * @copyright Copyright (c) 2016 Anatolii Pipko
+ * @license   https://github.com/tolik505/yii2-related-form/blob/master/LICENSE
+ */
+
+namespace tolik505\relatedform;
+
+/**
+ * Asset bundle for relatedform Widget
+ **/
+class RelatedFormAsset extends \yii\web\AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\widgets\ActiveFormAsset'
+    ];
+
+    /**
+     * Set up CSS and JS asset arrays based on the base-file names
+     * @param string $type whether 'css' or 'js'
+     * @param array $files the list of 'css' or 'js' basefile names
+     */
+    protected function setupAssets($type, $files = [])
+    {
+        $srcFiles = [];
+        $minFiles = [];
+        foreach ($files as $file) {
+            $srcFiles[] = "{$file}.{$type}";
+            $minFiles[] = "{$file}.min.{$type}";
+        }
+        if (empty($this->$type)) {
+            $this->$type = YII_DEBUG ? $srcFiles : $minFiles;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('js', ['yii2-dynamic-form']);
+        parent::init();
+    }
+
+    /**
+     * Sets the source path if empty
+     * @param string $path the path to be set
+     */
+    protected function setSourcePath($path)
+    {
+        if (empty($this->sourcePath)) {
+            $this->sourcePath = $path;
+        }
+    }
+}
